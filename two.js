118924b244383c71374e7f724364b054acffb2d7
Paul <mr.firo@ya.ru>
2020-01-08 15:15:42 +0300

    Это наш первый комит

---
+++ two.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var number = 5;
+var string = "Hello"
+var sym = Symbol();
+var boolean = true;
+null
+undefined
+var obj = {};
+
+
+let pers = {
+    name: "Paul",
+    age: 25,
+    isMarried: false
+}
+
+let incr =10,
+    decr =10;
+
+    console.log(~(10));
+//incr++
+//decr--;
+//console.log(++incr);
+//console.log(--decr);
+/*
+var ans = +prompt("Сколько Вам лет?");
+console.log(typeof(ans));
+console.log(ans);
+ans = +ans;
+console.log(typeof(ans));
+console.log(ans);
+*/
+//let a = ['i.jpg','i1.jpg','i2.jpg']
+//pers.age = prompt("Сколько Вам лет?");
+//console.log(typeof(a));
+//alert(number); 
+/*var ans = confirm(string);
+console.log(ans);*/
